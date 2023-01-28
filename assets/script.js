fc4b425e855812557f18165706337732e527c57b
reets <117296082+reets93@users.noreply.github.com>
2023-01-28 00:35:23 +0000

    added 5 day forecasts. created day 1 forecast then copied and amended days2-day5. styled the daily forecast divs

--- assets/script.js
+++ assets/script.js
@@ -55,32 +55,137 @@ $('#search-button').on("click", function weatherData(e) {
         todayDetailsHumidity.text("Humidity: " + todayHumidity + "%")
         $("#today").append(todayDetailsHumidity)
 
-        $("#today").css({border: "solid 1px grey", padding: "8px"})
-      
+        $("#today").css({ border: "solid 1px grey", padding: "8px" })
+
         // create element for forecast heading
         var forecastTitle = $('<h4>')
         forecastTitle.text("5-Day Forecast:")
         $("#forecast-title").append(forecastTitle)
 
-        // day1 forecast div
-        // var dforecast = $('<div>').css({border: "solid 1px red"})
-        // $('#forecast').append(forecast)
-
         // create daily forecasts
-        var day1 = $('<div>').attr("id","day1")
-        var day1Date = $('<p>')
-        day1Date.text("text")
-        $(day1).append(day1Date)
-        //date <h5> moment(). add(15,'days'). format('DD-MM-YYYY')
-        //icon <img>
-        //temp <p>
-        //wind <p>
-        //humidity <p>
-        
-        
-        
-        $("#forecast").append(day1)
+        // day 1 forecast
+        var day1 = $('<div>').attr("id", "day1").css({ "margin-left": "12px", padding: "20px", color: "white", "background-color": "#0A3668" })
 
+        var day1Date = $('<h6>')
+        day1Date.text(moment().add(1, 'day').format("D-MM-YY"))
+
+        var day1Icon = response.list[12].weather[0].icon
+        var day1IconURL = "https://openweathermap.org/img/wn/" + day1Icon + ".png"
+        var day1Img = $('<img>')
+        day1Img.attr("src", day1IconURL)
+
+        var day1Temp = $('<p>')
+        var day1Celsius = Math.floor(response.list[12].main.temp - 273.15)
+        day1Temp.text("Temp: " + day1Celsius + "°C")
+
+        var day1Wind = $('<p>')
+        day1Wind.text("Wind: " + response.list[12].wind.speed + " kph")
+
+        var day1Humidity = $('<p>')
+        day1Humidity.text("Humidity: " + response.list[12].main.humidity + "%")
+
+        day1.append(day1Date).append(day1Img).append(day1Temp).append(day1Wind).append(day1Humidity)
+        $("#forecast").prepend(day1)
+
+
+        // day 2 forecast
+        var day2 = $('<div>').attr("id", "day1").css({ "margin-left": "12px", padding: "20px", color: "white", "background-color": "#0A3668" })
+
+        var day2Date = $('<h6>')
+        day2Date.text(moment().add(2, 'days').format("D-MM-YY"))
+
+        var day2Icon = response.list[20].weather[0].icon
+        var day2IconURL = "https://openweathermap.org/img/wn/" + day2Icon + ".png"
+        var day2Img = $('<img>')
+        day2Img.attr("src", day2IconURL)
+
+        var day2Temp = $('<p>')
+        var day2Celsius = Math.floor(response.list[20].main.temp - 273.15)
+        day2Temp.text("Temp: " + day2Celsius + "°C")
+
+        var day2Wind = $('<p>')
+        day2Wind.text("Wind: " + response.list[20].wind.speed + " kph")
+
+        var day2Humidity = $('<p>')
+        day2Humidity.text("Humidity: " + response.list[20].main.humidity + "%")
+
+        day2.append(day2Date).append(day2Img).append(day2Temp).append(day2Wind).append(day2Humidity)
+        $("#forecast").append(day2)
+
+        // day 3 forecast
+        var day3 = $('<div>').attr("id", "day1").css({ "margin-left": "12px", padding: "20px", color: "white", "background-color": "#0A3668" })
+
+        var day3Date = $('<h6>')
+        day3Date.text(moment().add(3, 'days').format("D-MM-YY"))
+
+        var day3Icon = response.list[28].weather[0].icon
+        var day3IconURL = "https://openweathermap.org/img/wn/" + day3Icon + ".png"
+        var day3Img = $('<img>')
+        day3Img.attr("src", day3IconURL)
+
+        var day3Temp = $('<p>')
+        var day3Celsius = Math.floor(response.list[28].main.temp - 273.15)
+        day3Temp.text("Temp: " + day3Celsius + "°C")
+
+        var day3Wind = $('<p>')
+        day3Wind.text("Wind: " + response.list[28].wind.speed + " kph")
+
+        var day3Humidity = $('<p>')
+        day3Humidity.text("Humidity: " + response.list[28].main.humidity + "%")
+
+        day3.append(day3Date).append(day3Img).append(day3Temp).append(day3Wind).append(day3Humidity)
+        $("#forecast").append(day3)
+
+        // day 4 forecast
+        var day4 = $('<div>').attr("id", "day1").css({ "margin-left": "12px", padding: "20px", color: "white", "background-color": "#0A3668" })
+
+        var day4Date = $('<h6>')
+        day4Date.text(moment().add(4,'days').format("D-MM-YY"))
+
+        var day4Icon = response.list[36].weather[0].icon
+        var day4IconURL = "https://openweathermap.org/img/wn/" + day4Icon + ".png"
+        var day4Img = $('<img>')
+        day4Img.attr("src", day4IconURL)
+
+        var day4Temp = $('<p>')
+        var day4Celsius = Math.floor(response.list[36].main.temp - 273.15)
+        day4Temp.text("Temp: " + day4Celsius + "°C")
+
+        var day4Wind = $('<p>')
+        day4Wind.text("Wind: " + response.list[36].wind.speed + " kph")
+
+        var day4Humidity = $('<p>')
+        day4Humidity.text("Humidity: " + response.list[36].main.humidity + "%")
+
+        day4.append(day4Date).append(day4Img).append(day4Temp).append(day4Wind).append(day4Humidity)
+        $("#forecast").append(day4)
+
+
+        // day 5 forecast
+        var day5 = $('<div>').attr("id", "day1").css({ "margin-left": "12px", padding: "20px", color: "white", "background-color": "#0A3668" })
+
+        var day5Date = $('<h6>')
+        day5Date.text(moment().add(5,'days').format("D-MM-YY"))
+
+        var day5Icon = response.list[39].weather[0].icon
+        var day5IconURL = "https://openweathermap.org/img/wn/" + day5Icon + ".png"
+        var day5Img = $('<img>')
+        day5Img.attr("src", day5IconURL)
+
+        var day5Temp = $('<p>')
+        var day5Celsius = Math.floor(response.list[39].main.temp - 273.15)
+        day5Temp.text("Temp: " + day5Celsius + "°C")
+
+        var day5Wind = $('<p>')
+        day5Wind.text("Wind: " + response.list[39].wind.speed + " kph")
+
+        var day5Humidity = $('<p>')
+        day5Humidity.text("Humidity: " + response.list[39].main.humidity + "%")
+
+        day5.append(day5Date).append(day5Img).append(day5Temp).append(day5Wind).append(day5Humidity)
+        $("#forecast").append(day5)
     })
 
 })
+
+// how could a for-loop work instead of above?
