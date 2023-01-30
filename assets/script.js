74672f3cd0b8b1314f75c6ae9550bbbc47852f9a
reets <117296082+reets93@users.noreply.github.com>
2023-01-30 16:20:52 +0000

    added a historical btn on click outside of main code block which seems to work nicely for local storage items. however anything generated in current session not clicking as intended

--- assets/script.js
+++ assets/script.js
@@ -8,12 +8,11 @@ var searchCity;
 var historySearch = []
 
 // persist local storage
-for (i = 0; i < localStorage.length; i++) { //original code
+for (i = 0; i < localStorage.length; i++) {
     var histBtn = $('<button>').addClass("historical-btn")
     histBtn.text(localStorage.getItem("city" + [i])).css({ "background-color": "##D5E8F6", color: "#474954", "border-radius": "4px", "margin-top": "8px" })
     $("#history").append(histBtn)
 }
-
 
 // event lister for submit button
 $('#search-button').on("click", function weatherData(e) {
@@ -99,47 +98,48 @@ $('#search-button').on("click", function weatherData(e) {
             $("#forecast-title").append(forecastTitle)
         }
 
-        // Weather details for 5-day forecast
-        var fiveForecast = [
-            {
-                date: moment().add(1, 'days').format("D-MM-YY"),
-                icon: "https://openweathermap.org/img/wn/" + response.list[12].weather[0].icon + ".png",
-                temp: Math.floor(response.list[12].main.temp - 273.15),
-                wind: response.list[12].wind.speed,
-                humidity: response.list[12].main.humidity,
-            },
-            {
-                date: moment().add(2, 'days').format("D-MM-YY"),
-                icon: "https://openweathermap.org/img/wn/" + response.list[20].weather[0].icon + ".png",
-                temp: Math.floor(response.list[20].main.temp - 273.15),
-                wind: response.list[20].wind.speed,
-                humidity: response.list[20].main.humidity,
-            },
-            {
-                date: moment().add(3, 'days').format("D-MM-YY"),
-                icon: "https://openweathermap.org/img/wn/" + response.list[28].weather[0].icon + ".png",
-                temp: Math.floor(response.list[28].main.temp - 273.15),
-                wind: response.list[28].wind.speed,
-                humidity: response.list[28].main.humidity,
-            },
-            {
-                date: moment().add(4, 'days').format("D-MM-YY"),
-                icon: "https://openweathermap.org/img/wn/" + response.list[36].weather[0].icon + ".png",
-                temp: Math.floor(response.list[36].main.temp - 273.15),
-                wind: response.list[36].wind.speed,
-                humidity: response.list[36].main.humidity,
-            },
-            {
-                date: moment().add(5, 'days').format("D-MM-YY"),
-                icon: "https://openweathermap.org/img/wn/" + response.list[39].weather[0].icon + ".png",
-                temp: Math.floor(response.list[39].main.temp - 273.15),
-                wind: response.list[39].wind.speed,
-                humidity: response.list[39].main.humidity,
-            }
-        ]
 
         // function that creates 5 day forecast 
         function dailyForecast() {
+            // Weather details for 5-day forecast
+            var fiveForecast = [
+                {
+                    date: moment().add(1, 'days').format("D-MM-YY"),
+                    icon: "https://openweathermap.org/img/wn/" + response.list[12].weather[0].icon + ".png",
+                    temp: Math.floor(response.list[12].main.temp - 273.15),
+                    wind: response.list[12].wind.speed,
+                    humidity: response.list[12].main.humidity,
+                },
+                {
+                    date: moment().add(2, 'days').format("D-MM-YY"),
+                    icon: "https://openweathermap.org/img/wn/" + response.list[20].weather[0].icon + ".png",
+                    temp: Math.floor(response.list[20].main.temp - 273.15),
+                    wind: response.list[20].wind.speed,
+                    humidity: response.list[20].main.humidity,
+                },
+                {
+                    date: moment().add(3, 'days').format("D-MM-YY"),
+                    icon: "https://openweathermap.org/img/wn/" + response.list[28].weather[0].icon + ".png",
+                    temp: Math.floor(response.list[28].main.temp - 273.15),
+                    wind: response.list[28].wind.speed,
+                    humidity: response.list[28].main.humidity,
+                },
+                {
+                    date: moment().add(4, 'days').format("D-MM-YY"),
+                    icon: "https://openweathermap.org/img/wn/" + response.list[36].weather[0].icon + ".png",
+                    temp: Math.floor(response.list[36].main.temp - 273.15),
+                    wind: response.list[36].wind.speed,
+                    humidity: response.list[36].main.humidity,
+                },
+                {
+                    date: moment().add(5, 'days').format("D-MM-YY"),
+                    icon: "https://openweathermap.org/img/wn/" + response.list[39].weather[0].icon + ".png",
+                    temp: Math.floor(response.list[39].main.temp - 273.15),
+                    wind: response.list[39].wind.speed,
+                    humidity: response.list[39].main.humidity,
+                }
+            ]
+
             console.log(fiveForecast)
 
             for (let i = 0; i < fiveForecast.length; i++) {
@@ -176,7 +176,7 @@ $('#search-button').on("click", function weatherData(e) {
         //call the daily forecast function
         dailyForecast()
 
-        //on click for historical button
+        // on click for historical button
         $('.historical-btn').on('click', function (button) {
             button.stopPropagation()
             searchCity = $(button.target).text()
@@ -201,6 +201,143 @@ $('#search-button').on("click", function weatherData(e) {
 
 })
 
+$('.historical-btn').on('click', function (button) {
+    var content = $(button.target).text()
+    console.log("hist-button working " + content)
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + content + "&appid=" + apiKEY
+    console.log("link working")
+
+    //ajax function
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        $('#today').empty()
+        $('#forecast-title').empty()
+        $('#forecast').empty()
+        todayWeather()
+        dailyForecast()
+
+        // function to display current/today weather forecast
+        function todayWeather() {
+            // today's weather data
+
+            // $('#today').empty()
+
+            var today =
+            {
+                date: moment().format("(D MMM YYYY)"),
+                city: response.city.name,
+                celsius: Math.floor(response.list[0].main.temp - 273.15),
+                wind: response.list[0].wind.speed,
+                humidity: response.list[0].main.humidity,
+                icon: "https://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png"
+            }
+
+            // create h2 element for today's city + date
+            var todayTitle = $('<h2>')
+            todayTitle.text(today.city + " " + today.date)
+
+            // create current weather icon
+            var currentIcon = $('<img>')
+            currentIcon.attr("src", today.icon)
+            todayTitle.append(currentIcon)
+
+            //create p elements for today's weather data
+            var currentTemp = $('<p>')
+            currentTemp.text("Temp: " + today.celsius + "°C")
+
+            var currentWind = $('<p>')
+            currentWind.text("Wind: " + today.wind + " KPH")
+
+            var currentHumidity = $('<p>')
+            currentHumidity.text("Humidity: " + today.humidity + "%")
+
+            // styling and append details to today section
+            $("#today").css({ border: "solid 1px grey", padding: "8px" }).append(todayTitle).append(currentTemp).append(currentWind).append(currentHumidity)
+
+            // create element for forecast heading
+            var forecastTitle = $('<h4>')
+            forecastTitle.text("5-Day Forecast:")
+            $("#forecast-title").append(forecastTitle)
+        }
+
+        // function that creates 5 day forecast 
+        function dailyForecast() {
+            // Weather details for 5-day forecast
+            var fiveForecast = [
+                {
+                    date: moment().add(1, 'days').format("D-MM-YY"),
+                    icon: "https://openweathermap.org/img/wn/" + response.list[12].weather[0].icon + ".png",
+                    temp: Math.floor(response.list[12].main.temp - 273.15),
+                    wind: response.list[12].wind.speed,
+                    humidity: response.list[12].main.humidity,
+                },
+                {
+                    date: moment().add(2, 'days').format("D-MM-YY"),
+                    icon: "https://openweathermap.org/img/wn/" + response.list[20].weather[0].icon + ".png",
+                    temp: Math.floor(response.list[20].main.temp - 273.15),
+                    wind: response.list[20].wind.speed,
+                    humidity: response.list[20].main.humidity,
+                },
+                {
+                    date: moment().add(3, 'days').format("D-MM-YY"),
+                    icon: "https://openweathermap.org/img/wn/" + response.list[28].weather[0].icon + ".png",
+                    temp: Math.floor(response.list[28].main.temp - 273.15),
+                    wind: response.list[28].wind.speed,
+                    humidity: response.list[28].main.humidity,
+                },
+                {
+                    date: moment().add(4, 'days').format("D-MM-YY"),
+                    icon: "https://openweathermap.org/img/wn/" + response.list[36].weather[0].icon + ".png",
+                    temp: Math.floor(response.list[36].main.temp - 273.15),
+                    wind: response.list[36].wind.speed,
+                    humidity: response.list[36].main.humidity,
+                },
+                {
+                    date: moment().add(5, 'days').format("D-MM-YY"),
+                    icon: "https://openweathermap.org/img/wn/" + response.list[39].weather[0].icon + ".png",
+                    temp: Math.floor(response.list[39].main.temp - 273.15),
+                    wind: response.list[39].wind.speed,
+                    humidity: response.list[39].main.humidity,
+                }
+            ]
+            console.log(fiveForecast)
+
+            for (let i = 0; i < fiveForecast.length; i++) {
+                // create div
+                var day = $('<div>').css({ "margin-left": "12px", padding: "20px", color: "white", "background-color": "#0A3668" })
+                $("#forecast").append(day)
+
+                //create date
+                var date = $('<h6>')
+                date.text(fiveForecast[i].date)
+
+                //create icon
+                var icon = $('<img>')
+                icon.attr("src", fiveForecast[i].icon)
+
+                //create temp
+                var temp = $('<p>')
+                temp.text("Temp: " + fiveForecast[i].temp + "°C")
+
+                //create wind
+                var wind = $('<p>')
+                wind.text("Wind: " + fiveForecast[i].wind + " kph")
+
+                //create humidity
+                var humidity = $('<p>')
+                humidity.text("Humidity: " + fiveForecast[i].humidity + "%")
+
+                //append
+                day.append(date).append(icon).append(temp).append(wind).append(humidity)
+            }
+
+        }
+
+    })
+})
+
 // validate city input
 // validate api key? 
-// link button to the search city 
