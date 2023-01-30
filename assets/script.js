c80b898a4396c52087818cf6b1f873e8eca7c1ff
reets <117296082+reets93@users.noreply.github.com>
2023-01-30 20:59:14 +0000

    took functions out of main ajax/then code blocks. passed responses through to other functions. Has not fixed issue for clicking on latest created buttons and linking to search

--- assets/script.js
+++ assets/script.js
@@ -39,8 +39,6 @@ $('#search-button').on("click", function weatherData(e) {
         //clears search input after submit
         $('#search-input').val('')
 
-        //validate city? 
-
         //creates historical button 
         var searchInput = $('<button>').addClass("historical-btn")
         searchInput.text(cityName).css({ "background-color": "##D5E8F6", color: "#474954", "border-radius": "4px", "margin-top": "8px" })
@@ -50,158 +48,141 @@ $('#search-button').on("click", function weatherData(e) {
 
 
         store()
-        function store() {
-            for (let i = 0; i < historySearch.length; i++) {
-                localStorage.setItem("city" + [i], historySearch[i])
-            }
-        }
 
 
-        todayWeather()
-        function todayWeather() {
-            // today's weather data
-            var today =
-            {
-                date: moment().format("(D MMM YYYY)"),
-                city: response.city.name,
-                celsius: Math.floor(response.list[0].main.temp - 273.15),
-                wind: response.list[0].wind.speed,
-                humidity: response.list[0].main.humidity,
-                icon: "https://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png"
-            }
+        todayWeather(response)
 
-            // create h2 element for today's city + date
-            var todayTitle = $('<h2>')
-            todayTitle.text(today.city + " " + today.date)
-
-            // create current weather icon
-            var currentIcon = $('<img>')
-            currentIcon.attr("src", today.icon)
-            todayTitle.append(currentIcon)
-
-            //create p elements for today's weather data
-            var currentTemp = $('<p>')
-            currentTemp.text("Temp: " + today.celsius + "°C")
-
-            var currentWind = $('<p>')
-            currentWind.text("Wind: " + today.wind + " KPH")
-
-            var currentHumidity = $('<p>')
-            currentHumidity.text("Humidity: " + today.humidity + "%")
-
-            // styling and append details to today section
-            $("#today").css({ border: "solid 1px grey", padding: "8px" }).append(todayTitle).append(currentTemp).append(currentWind).append(currentHumidity)
-
-            // create element for forecast heading
-            var forecastTitle = $('<h4>')
-            forecastTitle.text("5-Day Forecast:")
-            $("#forecast-title").append(forecastTitle)
-        }
-
-
-        // function that creates 5 day forecast 
-        function dailyForecast() {
-            // Weather details for 5-day forecast
-            var fiveForecast = [
-                {
-                    date: moment().add(1, 'days').format("D-MM-YY"),
-                    icon: "https://openweathermap.org/img/wn/" + response.list[12].weather[0].icon + ".png",
-                    temp: Math.floor(response.list[12].main.temp - 273.15),
-                    wind: response.list[12].wind.speed,
-                    humidity: response.list[12].main.humidity,
-                },
-                {
-                    date: moment().add(2, 'days').format("D-MM-YY"),
-                    icon: "https://openweathermap.org/img/wn/" + response.list[20].weather[0].icon + ".png",
-                    temp: Math.floor(response.list[20].main.temp - 273.15),
-                    wind: response.list[20].wind.speed,
-                    humidity: response.list[20].main.humidity,
-                },
-                {
-                    date: moment().add(3, 'days').format("D-MM-YY"),
-                    icon: "https://openweathermap.org/img/wn/" + response.list[28].weather[0].icon + ".png",
-                    temp: Math.floor(response.list[28].main.temp - 273.15),
-                    wind: response.list[28].wind.speed,
-                    humidity: response.list[28].main.humidity,
-                },
-                {
-                    date: moment().add(4, 'days').format("D-MM-YY"),
-                    icon: "https://openweathermap.org/img/wn/" + response.list[36].weather[0].icon + ".png",
-                    temp: Math.floor(response.list[36].main.temp - 273.15),
-                    wind: response.list[36].wind.speed,
-                    humidity: response.list[36].main.humidity,
-                },
-                {
-                    date: moment().add(5, 'days').format("D-MM-YY"),
-                    icon: "https://openweathermap.org/img/wn/" + response.list[39].weather[0].icon + ".png",
-                    temp: Math.floor(response.list[39].main.temp - 273.15),
-                    wind: response.list[39].wind.speed,
-                    humidity: response.list[39].main.humidity,
-                }
-            ]
-
-            console.log(fiveForecast)
-
-            for (let i = 0; i < fiveForecast.length; i++) {
-                // create div
-                var day = $('<div>').css({ "margin-left": "12px", padding: "20px", color: "white", "background-color": "#0A3668" })
-                $("#forecast").append(day)
-
-                //create date
-                var date = $('<h6>')
-                date.text(fiveForecast[i].date)
-
-                //create icon
-                var icon = $('<img>')
-                icon.attr("src", fiveForecast[i].icon)
-
-                //create temp
-                var temp = $('<p>')
-                temp.text("Temp: " + fiveForecast[i].temp + "°C")
-
-                //create wind
-                var wind = $('<p>')
-                wind.text("Wind: " + fiveForecast[i].wind + " kph")
-
-                //create humidity
-                var humidity = $('<p>')
-                humidity.text("Humidity: " + fiveForecast[i].humidity + "%")
-
-                //append
-                day.append(date).append(icon).append(temp).append(wind).append(humidity)
-            }
-
-        }
 
         //call the daily forecast function
-        dailyForecast()
+        dailyForecast(response)
 
-        // on click for historical button
-        // $('.historical-btn').on('click', function (e) {
-        //     e.stopPropagation()
-        //     e.preventDefault() 
-        //     console.log("working")
-        //     searchCity = $(e.target).text()
-        //     console.log("checking " +searchCity)
-
-        //     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=" + apiKEY
-
-        //     $.ajax({
-        //         url: queryURL,
-        //         method: "GET"
-        //     }).then(function (reponse) {
-        //         console.log(response)
-        //         $('#today').empty()
-        //         $('#forecast-title').empty()
-        //         $("#forecast").empty()
-        //         todayWeather()
-        //         dailyForecast()
-        //     })
 
     }).catch(err => alert("Please enter a valid city name. Check spelling and try again."))
     //catch function learnt from a youtube video : https://www.youtube.com/watch?v=GXrDEA3SIOQ
+    // })
 })
-// })
+
+
+function store() {
+    for (let i = 0; i < historySearch.length; i++) {
+        localStorage.setItem("city" + [i], historySearch[i])
+    }
+}
+
+function todayWeather(response) {
+    // today's weather data
+    var today =
+    {
+        date: moment().format("(D MMM YYYY)"),
+        city: response.city.name,
+        celsius: Math.floor(response.list[0].main.temp - 273.15),
+        wind: response.list[0].wind.speed,
+        humidity: response.list[0].main.humidity,
+        icon: "https://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png"
+    }
+
+    // create h2 element for today's city + date
+    var todayTitle = $('<h2>')
+    todayTitle.text(today.city + " " + today.date)
+
+    // create current weather icon
+    var currentIcon = $('<img>')
+    currentIcon.attr("src", today.icon)
+    todayTitle.append(currentIcon)
+
+    //create p elements for today's weather data
+    var currentTemp = $('<p>')
+    currentTemp.text("Temp: " + today.celsius + "°C")
+
+    var currentWind = $('<p>')
+    currentWind.text("Wind: " + today.wind + " KPH")
+
+    var currentHumidity = $('<p>')
+    currentHumidity.text("Humidity: " + today.humidity + "%")
+
+    // styling and append details to today section
+    $("#today").css({ border: "solid 1px grey", padding: "8px" }).append(todayTitle).append(currentTemp).append(currentWind).append(currentHumidity)
+
+    // create element for forecast heading
+    var forecastTitle = $('<h4>')
+    forecastTitle.text("5-Day Forecast:")
+    $("#forecast-title").append(forecastTitle)
+}
+
+// function that creates 5 day forecast 
+function dailyForecast(response) {
+    // Weather details for 5-day forecast
+    var fiveForecast = [
+        {
+            date: moment().add(1, 'days').format("D-MM-YY"),
+            icon: "https://openweathermap.org/img/wn/" + response.list[12].weather[0].icon + ".png",
+            temp: Math.floor(response.list[12].main.temp - 273.15),
+            wind: response.list[12].wind.speed,
+            humidity: response.list[12].main.humidity,
+        },
+        {
+            date: moment().add(2, 'days').format("D-MM-YY"),
+            icon: "https://openweathermap.org/img/wn/" + response.list[20].weather[0].icon + ".png",
+            temp: Math.floor(response.list[20].main.temp - 273.15),
+            wind: response.list[20].wind.speed,
+            humidity: response.list[20].main.humidity,
+        },
+        {
+            date: moment().add(3, 'days').format("D-MM-YY"),
+            icon: "https://openweathermap.org/img/wn/" + response.list[28].weather[0].icon + ".png",
+            temp: Math.floor(response.list[28].main.temp - 273.15),
+            wind: response.list[28].wind.speed,
+            humidity: response.list[28].main.humidity,
+        },
+        {
+            date: moment().add(4, 'days').format("D-MM-YY"),
+            icon: "https://openweathermap.org/img/wn/" + response.list[36].weather[0].icon + ".png",
+            temp: Math.floor(response.list[36].main.temp - 273.15),
+            wind: response.list[36].wind.speed,
+            humidity: response.list[36].main.humidity,
+        },
+        {
+            date: moment().add(5, 'days').format("D-MM-YY"),
+            icon: "https://openweathermap.org/img/wn/" + response.list[39].weather[0].icon + ".png",
+            temp: Math.floor(response.list[39].main.temp - 273.15),
+            wind: response.list[39].wind.speed,
+            humidity: response.list[39].main.humidity,
+        }
+    ]
+
+    console.log(fiveForecast)
+
+    for (let i = 0; i < fiveForecast.length; i++) {
+        // create div
+        var day = $('<div>').css({ "margin-left": "12px", padding: "20px", color: "white", "background-color": "#0A3668" })
+        $("#forecast").append(day)
+
+        //create date
+        var date = $('<h6>')
+        date.text(fiveForecast[i].date)
+
+        //create icon
+        var icon = $('<img>')
+        icon.attr("src", fiveForecast[i].icon)
+
+        //create temp
+        var temp = $('<p>')
+        temp.text("Temp: " + fiveForecast[i].temp + "°C")
+
+        //create wind
+        var wind = $('<p>')
+        wind.text("Wind: " + fiveForecast[i].wind + " kph")
+
+        //create humidity
+        var humidity = $('<p>')
+        humidity.text("Humidity: " + fiveForecast[i].humidity + "%")
+
+        //append
+        day.append(date).append(icon).append(temp).append(wind).append(humidity)
+    }
+
+}
+
 
 $('.historical-btn').on('click', function searchHistory(button) {
     var content = $(button.target).text()
@@ -218,125 +199,8 @@ $('.historical-btn').on('click', function searchHistory(button) {
         $('#today').empty()
         $('#forecast-title').empty()
         $('#forecast').empty()
-        todayWeather()
-        dailyForecast()
-
-        // function to display current/today weather forecast
-        function todayWeather() {
-            // today's weather data
-
-            // $('#today').empty()
-
-            var today =
-            {
-                date: moment().format("(D MMM YYYY)"),
-                city: response.city.name,
-                celsius: Math.floor(response.list[0].main.temp - 273.15),
-                wind: response.list[0].wind.speed,
-                humidity: response.list[0].main.humidity,
-                icon: "https://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png"
-            }
-
-            // create h2 element for today's city + date
-            var todayTitle = $('<h2>')
-            todayTitle.text(today.city + " " + today.date)
-
-            // create current weather icon
-            var currentIcon = $('<img>')
-            currentIcon.attr("src", today.icon)
-            todayTitle.append(currentIcon)
-
-            //create p elements for today's weather data
-            var currentTemp = $('<p>')
-            currentTemp.text("Temp: " + today.celsius + "°C")
-
-            var currentWind = $('<p>')
-            currentWind.text("Wind: " + today.wind + " KPH")
-
-            var currentHumidity = $('<p>')
-            currentHumidity.text("Humidity: " + today.humidity + "%")
-
-            // styling and append details to today section
-            $("#today").css({ border: "solid 1px grey", padding: "8px" }).append(todayTitle).append(currentTemp).append(currentWind).append(currentHumidity)
-
-            // create element for forecast heading
-            var forecastTitle = $('<h4>')
-            forecastTitle.text("5-Day Forecast:")
-            $("#forecast-title").append(forecastTitle)
-        }
-
-        // function that creates 5 day forecast 
-        function dailyForecast() {
-            // Weather details for 5-day forecast
-            var fiveForecast = [
-                {
-                    date: moment().add(1, 'days').format("D-MM-YY"),
-                    icon: "https://openweathermap.org/img/wn/" + response.list[12].weather[0].icon + ".png",
-                    temp: Math.floor(response.list[12].main.temp - 273.15),
-                    wind: response.list[12].wind.speed,
-                    humidity: response.list[12].main.humidity,
-                },
-                {
-                    date: moment().add(2, 'days').format("D-MM-YY"),
-                    icon: "https://openweathermap.org/img/wn/" + response.list[20].weather[0].icon + ".png",
-                    temp: Math.floor(response.list[20].main.temp - 273.15),
-                    wind: response.list[20].wind.speed,
-                    humidity: response.list[20].main.humidity,
-                },
-                {
-                    date: moment().add(3, 'days').format("D-MM-YY"),
-                    icon: "https://openweathermap.org/img/wn/" + response.list[28].weather[0].icon + ".png",
-                    temp: Math.floor(response.list[28].main.temp - 273.15),
-                    wind: response.list[28].wind.speed,
-                    humidity: response.list[28].main.humidity,
-                },
-                {
-                    date: moment().add(4, 'days').format("D-MM-YY"),
-                    icon: "https://openweathermap.org/img/wn/" + response.list[36].weather[0].icon + ".png",
-                    temp: Math.floor(response.list[36].main.temp - 273.15),
-                    wind: response.list[36].wind.speed,
-                    humidity: response.list[36].main.humidity,
-                },
-                {
-                    date: moment().add(5, 'days').format("D-MM-YY"),
-                    icon: "https://openweathermap.org/img/wn/" + response.list[39].weather[0].icon + ".png",
-                    temp: Math.floor(response.list[39].main.temp - 273.15),
-                    wind: response.list[39].wind.speed,
-                    humidity: response.list[39].main.humidity,
-                }
-            ]
-            console.log(fiveForecast)
-
-            for (let i = 0; i < fiveForecast.length; i++) {
-                // create div
-                var day = $('<div>').css({ "margin-left": "12px", padding: "20px", color: "white", "background-color": "#0A3668" })
-                $("#forecast").append(day)
-
-                //create date
-                var date = $('<h6>')
-                date.text(fiveForecast[i].date)
-
-                //create icon
-                var icon = $('<img>')
-                icon.attr("src", fiveForecast[i].icon)
-
-                //create temp
-                var temp = $('<p>')
-                temp.text("Temp: " + fiveForecast[i].temp + "°C")
-
-                //create wind
-                var wind = $('<p>')
-                wind.text("Wind: " + fiveForecast[i].wind + " kph")
-
-                //create humidity
-                var humidity = $('<p>')
-                humidity.text("Humidity: " + fiveForecast[i].humidity + "%")
-
-                //append
-                day.append(date).append(icon).append(temp).append(wind).append(humidity)
-            }
-
-        }
+        todayWeather(response)
+        dailyForecast(response)
 
     })
 })
